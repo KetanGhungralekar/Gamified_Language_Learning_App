d3a266aa8628c74d674140b5a612788a9923503c
Ketan Ghungralekar <Ketan.Ghungralekar@iiitb.ac.in>
2024-11-08 19:18:50 +0530

    add Sidebar

--- lang-model/src/App.js
+++ lang-model/src/App.js
@@ -7,13 +7,11 @@ import { Sidebar } from "./Components/Sidebar/Sidebar";
 const App = () => {
     return (
         <Router>
-            <Box sx={{ display: "flex" }}>
-                {/* Sidebar Component */}
+            <Box sx={{ display: "flex" }}>                
                 <Sidebar />
-                
                 {/* Main Content Area */}
                 <Box sx={{ flex: 1, padding: 3, backgroundColor: "#F3F4F6", minHeight: "100vh" }}>
-                    <AppRoutes />
+                    <AppRoutes /> {/* Will render Home by default */}
                 </Box>
             </Box>
         </Router>
--- lang-model/src/Components/Routers/UserRoute.jsx
+++ lang-model/src/Components/Routers/UserRoute.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../Home";
 import Profile from "../Profile";
 
 const AppRoutes = () => {
     return (
         <Routes>
+            {/* Default route to Home */}
+            <Route path="/" element={<Home />} />
+            
+            {/* Admin-specific routes */}
             <Route path="/admin/home" element={<Home />} />
             <Route path="/admin/profile" element={<Profile />} />
+
+            {/* Catch-all route to redirect to Home if route not found */}
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 };
